2f2b182d5f24f0bc64fc7ec6a616c24a2c7b6a5e
Javier Marin <jjmarin777@gmail.com>
2021-05-18 19:47:55 -0500

    commit tree 2a636aac118b5f586a8cc7de838ede7fa67a272f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_people)
+     array_of_people.map! do |people|
+         "Hello, my name is #{people}."
+    end
+end
+
+
+def assign_rooms(array_of_people)
+  
+    array_of_people.map.with_index(1) do |people,room|
+
+     "Hello, #{people}! You'll be assigned to room #{room}!"
+    end
+
+end
+
+def printer(array_of_people)
+
+    batch_badge_creator(array_of_people).each do |people|
+        puts people
+        # binding.pry
+    end
+    assign_rooms(array_of_people.each).each do |room|
+        puts room
+    end
+end
+
+
+
